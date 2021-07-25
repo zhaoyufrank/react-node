90bd02a4ca4bb45d513be35dd0ef31dac2afd6ae
Yu Zhao <zhaoyufrank8825@gmail.com>
2021-07-25 17:57:09 -0500

    backend

---
+++ backend/service/register.js
@@ -0,0 +1,83 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+const util = require("../utils/util");
+const bcrypt = require("bcryptjs");
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const userTable = "users";
+
+async function register(userInfo) {
+  const name = userInfo.name;
+  const email = userInfo.email;
+  const username = userInfo.username;
+  const password = userInfo.password;
+  if (!username || !name || !email || !password) {
+    return util.buildResponse(401, {
+      message: "All fields are required",
+    });
+  }
+
+  const dynamoUser = await getUser(username.toLowerCase().trim());
+  if (dynamoUser && dynamoUser.username) {
+    return util.buildResponse(401, {
+      message: "username already exist",
+    });
+  }
+
+  const encryptedPW = bcrypt.hashSync(password.trim(), 10);
+  const user = {
+    name: name,
+    email: email,
+    username: username.toLowerCase().trim(),
+    password: encryptedPW,
+  };
+
+  const saveUserResponse = await saveUser(user);
+  if (!saveUserResponse) {
+    return util.buildResponse(503, { message: "Server Error" });
+  }
+
+  return util.buildResponse(200, { username: username });
+}
+
+async function getUser(username) {
+  const params = {
+    TableName: userTable,
+    Key: {
+      username: username,
+    },
+  };
+
+  return await dynamodb
+    .get(params)
+    .promise()
+    .then(
+      (response) => {
+        return response.Item;
+      },
+      (error) => {
+        console.error("There is an error get user: ", error);
+      }
+    );
+}
+
+async function saveUser(user) {
+  const params = {
+    TableName: userTable,
+    Item: user,
+  };
+
+  return await dynamodb
+    .put(params)
+    .promise()
+    .then(
+      () => {
+        return true;
+      },
+      (error) => {
+        console.error("There is an error saving user: ", error);
+      }
+    );
+}
+
+module.exports.register = register;
